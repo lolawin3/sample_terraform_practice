4a00a4033ed038b742bfb918ad70d493ef878ffc
Luqman Olawin <luqmanolawin@Luqmans-MBP.attlocal.net>
2024-02-18 16:01:33 -0500

    adding environmental variables to ec2 config module

--- ec2.tf
+++ ec2.tf
@@ -13,6 +13,8 @@ terraform {
 
 provider "aws" {
     region = "us-east-1"
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 resource "aws_instance" "ec2" {
@@ -22,4 +24,12 @@ resource "aws_instance" "ec2" {
     tags = {
     Name = "TerraformSampleAppServerInstance"
     }
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+    default = ""
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+    default = ""
 }
